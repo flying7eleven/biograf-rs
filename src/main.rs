55c722acefc735d9ae8921a00815dee30048400a
Tim Janke <tim+github@janke.biz>
2021-08-30 17:28:49 +0200

    :sparkles: Add functionality to choose log verbosity

--- src/main.rs
+++ src/main.rs
@@ -12,6 +12,66 @@ struct Biograf {
     verbose: i32,
 }
 
+fn setup_logging(verbosity_level: i32) {
+    use chrono::Utc;
+
+    // create an instance for the Dispatcher to create a new logging configuration
+    let mut base_config = fern::Dispatch::new();
+
+    // determine the logging level based on the verbosity the user chose
+    base_config = match verbosity_level {
+        0 => base_config.level(log::LevelFilter::Warn),
+        1 => base_config.level(log::LevelFilter::Info),
+        2 => base_config.level(log::LevelFilter::Debug),
+        _3_or_more => base_config.level(log::LevelFilter::Trace),
+    };
+
+    // define how a logging line in the logfile should look like
+    let file_config = fern::Dispatch::new()
+        .format(|out, message, record| {
+            out.finish(format_args!(
+                "{}[{}][{}] {}",
+                Utc::now().format("[%Y-%m-%d][%H:%M:%S]"),
+                record.target(),
+                record.level(),
+                message
+            ))
+        })
+        .chain(fern::log_file("biograf.log").unwrap());
+
+    // define how a logging line on the console should look like
+    let stdout_config = fern::Dispatch::new()
+        .format(|out, message, record| {
+            out.finish(format_args!(
+                "{}[{}][{}] {}",
+                Utc::now().format("[%Y-%m-%d][%H:%M:%S]"),
+                record.target(),
+                record.level(),
+                message
+            ))
+        })
+        .chain(std::io::stdout());
+
+    // now chain everything together and get ready for actually logging stuff
+    base_config
+        .chain(file_config)
+        .chain(stdout_config)
+        .apply()
+        .unwrap();
+}
+
 fn main() {
-    Biograf::parse();
+    use log::{debug, error, info, trace, warn};
+
+    // get the command line parameters from the user
+    let cmd_parameters: Biograf = Biograf::parse();
+
+    // determine the verbosity level based on the occurrences of the flag
+    setup_logging(cmd_parameters.verbose);
+
+    trace!("Trace");
+    debug!("Debug");
+    info!("Info");
+    warn!("Warning");
+    error!("Error");
 }
