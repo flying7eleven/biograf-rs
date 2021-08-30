032a0d00c4ae4e3f15df31e213f8012a5d82a61a
Tim Janke <tim+github@janke.biz>
2021-08-30 17:10:56 +0200

    :construction: Add the first command line parsing to the CLI

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use clap::{crate_authors, crate_description, crate_name, crate_version, AppSettings, Clap};
+
+#[derive(Clap)]
+#[clap(setting = AppSettings::ColoredHelp)]
+#[clap(name = crate_name!(), version = crate_version!(), author = crate_authors!(), about = crate_description!())]
+struct Biograf {
+    /// Do not really modify the data, just simulate based on the input data
+    #[clap(short, long)]
+    dry_run: bool,
+    /// A level of verbosity, and can be used multiple times
+    #[clap(short, long, parse(from_occurrences))]
+    verbose: i32,
+}
+
 fn main() {
-    println!("Hello, world!");
+    Biograf::parse();
 }
