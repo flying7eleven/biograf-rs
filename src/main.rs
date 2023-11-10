2c16cfa8839fef5b9421b0d0ed77d9c6d0b8a247
Tim Janke <tim+github@janke.biz>
2023-11-10 11:11:53 +0100

    :fire: Remove all the temporary UI code

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,95 @@
-use biograf_rs::app::{App, AppResult};
-use biograf_rs::event::{Event, EventHandler};
-use biograf_rs::handler::handle_key_events;
-use biograf_rs::tui::Tui;
-use ratatui::backend::CrosstermBackend;
-use ratatui::Terminal;
-use std::io;
+use lazy_static::lazy_static;
+use google_youtube3::{YouTube, oauth2, hyper, hyper_rustls, chrono, Result, Error};
+use log::{debug, error, info};
 
-fn main() -> AppResult<()> {
-    let mut app = App::new();
+const GOOGLE_CLIENT_INFORMATION_STR: &str = include_str!("../client_secret.json");
+lazy_static! {
+    static ref GOOGLE_CLIENT_INFORMATION: google_youtube3::oauth2::ConsoleApplicationSecret =
+        serde_json::from_str(&GOOGLE_CLIENT_INFORMATION_STR).unwrap();
+}
 
-    let backend = CrosstermBackend::new(io::stderr());
-    let terminal = Terminal::new(backend)?;
-    let events = EventHandler::new(250);
-    let mut tui = Tui::new(terminal, events);
-    tui.init()?;
+fn setup_logging(verbosity_level: u8) {
+    use chrono::Utc;
 
-    while app.running {
-        tui.draw(&mut app)?;
-        match tui.events.next()? {
-            Event::Tick => app.tick(),
-            Event::Key(key_event) => handle_key_events(key_event, &mut app)?,
-            Event::Mouse(_) => {}
-            Event::Resize(_, _) => {}
+    // create an instance for the Dispatcher to create a new logging configuration
+    let mut base_config = fern::Dispatch::new();
+
+    // determine the logging level based on the verbosity the user chose
+    base_config = match verbosity_level {
+        0 => base_config.level(log::LevelFilter::Warn),
+        1 => base_config.level(log::LevelFilter::Info),
+        2 => base_config.level(log::LevelFilter::Debug),
+        _3_or_more => base_config.level(log::LevelFilter::Trace),
+    };
+
+    // define how a logging line in the logfile should look like
+    let file_config = fern::Dispatch::new()
+        .format(|out, message, record| {
+            out.finish(format_args!(
+                "{}[{}][{}] {}",
+                Utc::now().format("[%Y-%m-%d][%H:%M:%S]"),
+                record.target(),
+                record.level(),
+                message
+            ))
+        })
+        .chain(fern::log_file("biograf.log").unwrap());
+
+    // define how a logging line on the console should look like
+    let stdout_config = fern::Dispatch::new()
+        .format(|out, message, record| {
+            out.finish(format_args!(
+                "{}[{}][{}] {}",
+                Utc::now().format("[%Y-%m-%d][%H:%M:%S]"),
+                record.target(),
+                record.level(),
+                message
+            ))
+        })
+        .chain(std::io::stdout());
+
+    // now chain everything together and get ready for actually logging stuff
+    base_config
+        .chain(file_config)
+        .chain(stdout_config)
+        .apply()
+        .unwrap();
+}
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    setup_logging(2);
+
+    let auth = oauth2::InstalledFlowAuthenticator::builder(
+        GOOGLE_CLIENT_INFORMATION.installed.clone().unwrap(),
+        oauth2::InstalledFlowReturnMethod::HTTPRedirect,
+    ).build().await.unwrap();
+    let hub = YouTube::new(hyper::Client::builder().build(hyper_rustls::HttpsConnectorBuilder::new().with_native_roots().https_or_http().enable_http1().build()), auth);
+
+    let result = hub.playlists().list(&vec![]).mine(true).add_part( "snippet").doit().await;
+
+    match result {
+        Err(e) => match e {
+            Error::HttpError(_)
+            | Error::Io(_)
+            | Error::MissingAPIKey
+            | Error::MissingToken(_)
+            | Error::Cancelled
+            | Error::UploadSizeLimitExceeded(_, _)
+            | Error::Failure(_)
+            | Error::BadRequest(_)
+            | Error::FieldClash(_)
+            | Error::JsonDecodeError(_, _) => error!("{}", e),
+        },
+        Ok((_, res)) =>{
+
+            for current_list in res.items.unwrap() {
+
+                info!("{}", current_list.snippet.unwrap().title.unwrap());
+
+            }
         }
     }
 
-    tui.exit()?;
     Ok(())
 }
